5f8e0786e119a825fd5f7c925e9ff5cdb3c67b6b
morikann <mrknt0088@gmail.com>
2023-03-28 08:51:39 +0900

    Fix DeviceOrientation to PortraitUp

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_training/app.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp, // デバイス向きを縦に固定
+  ]);
+  
   runApp(const App());
 }
