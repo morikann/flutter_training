1d313bfe284aa90fe7b30ed5eecceb3f29098733
morikann <mrknt0088@gmail.com>
2023-03-27 15:05:28 +0900

    Add WeatherView which is Blank Page

---
+++ lib/view/weather_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class WeatherView extends StatelessWidget {
+  const WeatherView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('weather view'),
+      ),
+    );
+  }
+}
