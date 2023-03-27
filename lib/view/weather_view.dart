04173d4a51a07778b4f1bc96eeb0d82dabe41130
morikann <mrknt0088@gmail.com>
2023-03-27 18:26:09 +0900

    Implement WeatherView Layout

--- lib/view/weather_view.dart
+++ lib/view/weather_view.dart
@@ -5,9 +5,85 @@ class WeatherView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    final screenSize = MediaQuery.of(context).size;
+
+    return Scaffold(
       body: Center(
-        child: Text('weather view'),
+        child: Column(
+          children: [
+            const Spacer(),
+            SizedBox(
+              width: screenSize.width / 2,
+              child: Column(
+                children: [
+                  const AspectRatio(
+                    aspectRatio: 1,
+                    child: Placeholder(),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Text(
+                            '** ℃',
+                            textAlign: TextAlign.center,
+                            style: Theme.of(context)
+                                .textTheme
+                                .labelLarge
+                                ?.copyWith(
+                                  color: Colors.blue,
+                                ),
+                          ),
+                        ),
+                        Expanded(
+                          child: Text(
+                            '** ℃',
+                            textAlign: TextAlign.center,
+                            style: Theme.of(context)
+                                .textTheme
+                                .labelLarge
+                                ?.copyWith(
+                                  color: Colors.red,
+                                ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 80,
+                  ),
+                  SizedBox(
+                    width: screenSize.width / 2,
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: TextButton(
+                            onPressed: () {},
+                            child: const Text('Close'),
+                          ),
+                        ),
+                        Expanded(
+                          child: TextButton(
+                            onPressed: () {},
+                            child: const Text('Reload'),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
