8214bdf36ba7ebecb8241bc8466f652cce452a0c
morikann <mrknt0088@gmail.com>
2023-03-27 15:06:21 +0900

    Rename MaterialApp title

--- lib/app.dart
+++ lib/app.dart
@@ -7,7 +7,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Weather App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
