bf0c7f96ce93a3cb778c39059e5b0f51b8f6db45
morikann <mrknt0088@gmail.com>
2023-03-28 12:33:41 +0900

    Implement Screen Transition from LaunchView to WeatherView

--- lib/view/launch_view.dart
+++ lib/view/launch_view.dart
@@ -1,4 +1,6 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter_training/view/weather_view.dart';
 
 class LaunchView extends StatefulWidget {
   const LaunchView({super.key});
@@ -8,6 +10,21 @@ class LaunchView extends StatefulWidget {
 }
 
 class _LaunchViewState extends State<LaunchView> {
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance.endOfFrame.then((_) {
+      Future.delayed(const Duration(milliseconds: 500), () {
+        Navigator.of(context).push(
+          MaterialPageRoute<WeatherView>(
+            builder: (context) => const WeatherView(),
+          ),
+        );
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(color: Colors.green);
