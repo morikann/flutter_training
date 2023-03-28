4caa35425bc1f3954ea40a73aa24b7c4ab077b59
morikann <mrknt0088@gmail.com>
2023-03-28 12:00:02 +0900

    Add LaunchView with Green Background Color

---
+++ lib/view/launch_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LaunchView extends StatefulWidget {
+  const LaunchView({super.key});
+
+  @override
+  State<LaunchView> createState() => _LaunchViewState();
+}
+
+class _LaunchViewState extends State<LaunchView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.green);
+  }
+}
