7d64a04686828e8128512ec801cdc4719ea7ed38
morikann <mrknt0088@gmail.com>
2023-03-29 13:17:02 +0900

    Add WeatherPanel Widget

---
+++ lib/view/weather_view/weather_panel.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class WeatherPanel extends StatelessWidget {
+  const WeatherPanel({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const AspectRatio(
+      aspectRatio: 1,
+      child: Placeholder(),
+    );
+  }
+}
