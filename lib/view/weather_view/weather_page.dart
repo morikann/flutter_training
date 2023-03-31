52a28b0db8f7db2ee221872a3b2cb0d139ef7412
morikann <mrknt0088@gmail.com>
2023-03-31 15:16:53 +0900

    Pop WeatherPage when Close Button Tapped

--- lib/view/weather_view/weather_page.dart
+++ lib/view/weather_view/weather_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_training/model/weather.dart';
 import 'package:flutter_training/view/weather_view/component/weather_forecast.dart';
+import 'package:go_router/go_router.dart';
 import 'package:yumemi_weather/yumemi_weather.dart';
 
 final _weather = Weather(YumemiWeather());
@@ -37,8 +38,8 @@ class _WeatherPageState extends State<WeatherPage> {
                       children: [
                         Expanded(
                           child: TextButton(
+                            onPressed: context.pop,
                             child: const Text('Close'),
-                            onPressed: () {},
                           ),
                         ),
                         Expanded(
