06a93293cab1009e4d0acf6ef2deb4ebe6323b20
morikann <mrknt0088@gmail.com>
2023-04-13 10:52:21 +0900

    Add Result of Union Type

---
+++ lib/common/result.dart
@@ -0,0 +1,9 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'result.freezed.dart';
+
+@freezed
+class Result<T, E> with _$Result<T, E> {
+  const factory Result.success(T data) = Success<T, E>;
+  const factory Result.failure(E error) = Failure<T, E>;
+}
