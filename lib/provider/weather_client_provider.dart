2383b421108d736787501603a230677512b17e51
morikann <mrknt0088@gmail.com>
2023-04-13 10:51:29 +0900

    Delete provider directory

--- lib/provider/weather_client_provider.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:yumemi_weather/yumemi_weather.dart';
-
-final weatherClientProvider = Provider<YumemiWeather>((ref) => YumemiWeather());
--- lib/provider/weather_info_state_provider.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_training/model/weather/weather_info.dart';
-
-// 天気予報の状態を返すプロバイダ
-final weatherInfoStateProvider =
-    StateProvider.autoDispose<WeatherInfo?>((ref) => null);
