723ba990ded80a6da8ecb0f436d841479c9454b0
morikann <mrknt0088@gmail.com>
2023-04-20 09:37:37 +0900

    Add Doc Comments to weatherPageUiStateProvider

--- lib/view/weather/weather_page_ui_state.dart
+++ lib/view/weather/weather_page_ui_state.dart
@@ -3,7 +3,7 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'weather_page_ui_state.freezed.dart';
 
-// // WeatherPageの状態を返すプロバイダ
+/// WeatherPageの状態を返すプロバイダ
 final weatherPageUiStateProvider = StateProvider((ref) {
   return const WeatherPageUiState.init();
 });
