f11a3bf14a01bd4fa42a0e77900b0b8790b86fc6
morikann <mrknt0088@gmail.com>
2023-04-26 11:46:11 +0900

    Add Test of FetchWeatherUseCase when Result.failure('パラメータが間違っています。') is returned

--- test/unit/usecase/fetch_weather_use_case_test.dart
+++ test/unit/usecase/fetch_weather_use_case_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_training/common/constants/error_message.dart';
 import 'package:flutter_training/common/result.dart';
 import 'package:flutter_training/data/model/weather/weather_condition.dart';
 import 'package:flutter_training/data/model/weather/weather_forecast_target.dart';
@@ -7,6 +8,7 @@ import 'package:flutter_training/data/model/weather/weather_info.dart';
 import 'package:flutter_training/data/repository/weather_repository.dart';
 import 'package:flutter_training/data/usecase/fetch_weather_use_case.dart';
 import 'package:flutter_training/view/weather/component/weather_forecast.dart';
+import 'package:flutter_training/view/weather/weather_page_ui_state.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 
@@ -14,7 +16,7 @@ import 'fetch_weather_use_case_test.mocks.dart';
 
 // プロバイダによるListenerへの通知を追跡する
 class Listener<T> extends Mock {
-  void call(T? previous, T value);
+  void call(T? previous, T? value);
 }
 
 @GenerateNiceMocks([MockSpec<WeatherRepository>()])
@@ -81,5 +83,82 @@ void main() {
         verifyNoMoreInteractions(listener);
       },
     );
+    // 失敗ケース1
+    // Result.failure(error)が返ってきた時に、WeatherPageUiStateProviderの
+    // state(WeatherPageUiState)を受け取ったエラーメッセージの値
+    // （'パラメータが間違っています。'）で更新する
+    test('''
+      When Result.failure('パラメータが間違っています。') is returned,
+      update WeatherPageUiStateProvider state with the error message received
+    ''', () {
+      // Arrange
+      final mockRepository = MockWeatherRepository();
+      when(mockRepository.getWeather(any)).thenReturn(
+        const Result.failure(ErrorMessage.invalidParameter),
+      );
+
+      final container = ProviderContainer(
+        overrides: [
+          fetchWeatherUseCaseProvider.overrideWith(
+            (ref) => FetchWeatherUseCase(mockRepository, ref),
+          ),
+        ],
+      );
+
+      final listener = Listener<WeatherPageUiState>();
+
+      container.listen<WeatherPageUiState>(
+        weatherPageUiStateProvider,
+        listener,
+        fireImmediately: true,
+      );
+
+      verify(listener(null, const WeatherPageUiState.init())).called(1);
+      verifyNoMoreInteractions(listener);
+
+      expect(
+        container.read(weatherPageUiStateProvider),
+        const WeatherPageUiState.init(),
+      );
+
+      // Act
+      container.read(fetchWeatherUseCaseProvider).fetchWeather(
+            WeatherForecastTarget(
+              area: 'Tokyo',
+              date: DateTime.now(),
+            ),
+          );
+
+      // Assert
+      // WeatherPageUiStateはMutableなオブジェクトなので、オブジェクトではなく、型が等しいかテスト
+      expect(
+        container.read(weatherPageUiStateProvider),
+        isA<WeatherPageUiStateFailure>(),
+      );
+
+      verify(
+        listener(
+          const WeatherPageUiState.init(),
+          argThat(
+            isA<WeatherPageUiStateFailure>().having(
+              (failure) => failure.errorMessage,
+              'errorMessage',
+              ErrorMessage.invalidParameter,
+            ),
+          ),
+        ),
+      ).called(1);
+      verifyNoMoreInteractions(listener);
+    });
+
+    // 失敗ケース2
+    // Result.failure(error)が返ってきた時に、WeatherPageUiStateProviderの
+    // state(WeatherPageUiState)を受け取ったエラーメッセージの値
+    // （'予期せぬ不具合が発生しました。'）で更新する
+
+    // 失敗ケース3
+    // Result.failure(error)が返ってきた時に、WeatherPageUiStateProviderの
+    // state(WeatherPageUiState)を受け取ったエラーメッセージの値
+    // （'例外が発生しました。'）で更新する
   });
 }
