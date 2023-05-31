4e309281e52c5d1b08904ee8737fbeeff9ce9999
morikann <mrknt0088@gmail.com>
2023-05-31 13:10:29 +0900

    Refactor app_test to use device_size utils

--- test/widget/app_test.dart
+++ test/widget/app_test.dart
@@ -1,18 +1,19 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_training/app.dart';
 import 'package:flutter_training/view/launch_page.dart';
 import 'package:flutter_training/view/weather/weather_page.dart';
 
+import 'utils/device_size.dart';
+
 void main() {
+  setUp(setUpDeviceSize);
+  tearDown(tearDownDeviceSize);
+
   testWidgets('''
       Transition from LaunchPage to WeatherPage 
       after 500ms of application launch
     ''', (tester) async {
-    final binding = TestWidgetsFlutterBinding.ensureInitialized();
-    await binding.setSurfaceSize(const Size(1080, 1920));
-
     await tester.pumpWidget(
       const ProviderScope(
         child: App(),
@@ -20,9 +21,11 @@ void main() {
     );
 
     expect(find.byType(LaunchPage), findsOneWidget);
+    expect(find.byType(WeatherPage), findsNothing);
 
     await tester.pumpAndSettle(const Duration(milliseconds: 500));
 
     expect(find.byType(WeatherPage), findsOneWidget);
+    expect(find.byType(LaunchPage), findsNothing);
   });
 }
